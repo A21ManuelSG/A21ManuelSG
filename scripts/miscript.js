ea7a35d63c473065c03893a0c910f677fa467871
a21manuelsg <a21manuelsg@iessanclemente.net>
2022-09-14 20:08:35 +0200

    Primer commit

---
+++ scripts/miscript.js
@@ -0,0 +1,9 @@
+// console.log("Mi primer script")
+// num = prompt("Dame un numero")
+// datoBooleano = confirm("Si o no?")
+
+//document.write("Hola")
+
+let nombre = "Manu"
+console.log("Hola " + nombre)
+console.log(`Hola ${nombre}`) // Con Templates String, más recomendado
